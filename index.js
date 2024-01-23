1a12c720690bd05932eb372f84382d69a8986451
Kyle Michaels <kyle.michaels@gmail.com>
2024-01-22 18:24:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https:/
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            // If a specific origin isn't found on the list of allowed origins
-            let message = 'The CORS policy for this applications doesn\'t allow access from origin ' + origin;
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
             return callback(new Error(message), false);
         }
-        return callback(null, true); ''
+        return callback(null, true);
     }
 }));
 let auth = require('./auth')(app);
