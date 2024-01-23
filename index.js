ec8fcdbe8280558a58025da020d2b3872f6622a4
Kyle Michaels <kyle.michaels@gmail.com>
2024-01-22 18:14:31 -0800

    fixed issue of not hashing password for update user info

--- index.js
+++ index.js
@@ -140,12 +140,13 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), as
         return res.status(400).send('Permission denied');
     }
     // CONDITION ENDS
+    let hashedPassword = Users.hashPassword(req.body.Password);
     await Users.findOneAndUpdate({ Username: req.params.Username },
         {
             $set:
             {
                 Username: req.body.Username,
-                Password: req.body.Password,
+                Password: hashedPasswordPassword,
                 Email: req.body.Email,
                 Birthday: req.body.Birthday
             }
