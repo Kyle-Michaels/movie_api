10328b783de07447bbd003125171371351fab5cc
Kyle Michaels <kyle.michaels@gmail.com>
2024-01-22 18:35:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), as
             $set:
             {
                 Username: req.body.Username,
-                Password: hashedPasswordPassword,
+                Password: hashedPassword,
                 Email: req.body.Email,
                 Birthday: req.body.Birthday
             }
